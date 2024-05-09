9bff408f936c42f1c501b28aade9205883771086
Duel-A-ryan <133237907+Duel-A-ryan@users.noreply.github.com>
2024-05-09 16:38:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById('year').textContent = new year().getFullYear();
+function togglePDF() {
+    var pdfViewer = document.getElementById('pdfViewer');
+    if (pdfViewer.style.display === 'none') {
+        pdfViewer.style.display = 'block';
+    } else {
+        pdfViewer.style.display = 'none';
+    }
+}
