da5e1edc0f62f644f5b9291ae9faa0db9c2edd86
Duel-A-ryan <133237907+Duel-A-ryan@users.noreply.github.com>
2024-08-15 14:45:25 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function togglePDF() {
-    var pdfViewer = document.getElementById('pdfViewer');
-    if (pdfViewer.style.display === 'none') {
-        pdfViewer.style.display = 'block';
-    } else {
-        pdfViewer.style.display = 'none';
-    }
-}
